12053fdadd6e2df73ffbda0cd29e8eb3c598eeb5
Fiorillo <joao.banci@xbrain.com.br>
2024-03-12 19:41:25 -0300

    inicia projeto de autenticacao

---
+++ src/test/java/com/project/jooj/JoojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jooj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
